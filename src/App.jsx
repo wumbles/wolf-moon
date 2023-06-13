03c1222d1c6dd7b4afdd3f415b2e6ea5b926192f
wumbles <77227394+wumbles@users.noreply.github.com>
2023-06-12 20:41:07 -0600

    INITIAL COMMIT: working on a moon-related API react project

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+import { Moon } from "lunarphase-js";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [moonVisible, setMoonVisible] = useState(false);
+  const [quote, setQuote] = useState("");
+
+  const getMoonPhase = () => {
+    const currentPhase = Moon.lunarPhase() + " " + Moon.lunarPhaseEmoji();
+    console.log(currentPhase);
+
+    return (
+      <>
+        <h2>The 🐺 says today is:</h2>
+        <h3>{currentPhase}</h3>
+      </>
+    );
+  };
+
+  const getQuote = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+        "X-RapidAPI-Host": import.meta.env.VITE_RAPID_API_HOST,
+      },
+    };
+
+    const url =
+      "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info";
+    const response = await fetch(url, options);
+
+    const data = await response.json();
+
+    setQuote(data.text);
+  };
+
+  const showMoon = () => {
+    setMoonVisible(true);
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <button
+          onClick={() => {
+            showMoon();
+            // showQuote;
+          }}
+        >
+          <h1>Click for Moon Phase:</h1>
+        </button>
+      </div>
+      {moonVisible && getMoonPhase()}
+      <h4>{quote}</h4>
+    </>
+  );
+}
+
+export default App;
